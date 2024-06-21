cba7d4bfcf640868d32d922047836b78dad02170
Sejal Jagtap <sejaljagtap1309@gmail.com>
2024-06-22 01:07:39 +0530

    multiplayer canvas

--- app/page.tsx
+++ app/page.tsx
@@ -3,34 +3,37 @@
 import React, { FC } from 'react'
 import { useDraw } from '@/hooks/useDraw'
 import { ChromePicker } from 'react-color'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { drawLine } from '@/utils/drawLine'
+import { io } from 'socket.io-client'
+const socket = io('http://localhost:3001')
 interface PageProps { }
-
+type DrawLineProps = {
+  prevPoint: Point | null
+  currentPoint: Point
+  color: string
+}
 const Page: FC<PageProps> = ({ }) => {
-  const { canvasRef, onMouseDown, clear } = useDraw(drawLine);
+  const { canvasRef, onMouseDown, clear } = useDraw(createLine);
   const [color, setColor] = useState<string>("#000")
+  useEffect(() => {
+    const ctx = canvasRef.current?.getContext("2d")
+    socket.on('draw-line', ({ prevPoint, currentPoint, color }: DrawLineProps) => {
+      if (!ctx) return
+      drawLine({ prevPoint, currentPoint, ctx, color })
+    })
 
-  function drawLine({ prevPoint, currentPoint, ctx }: Draw) {
-    const { x: currX, y: currY } = currentPoint;
-    const lineColor = color;
-    const lineWidth = 5;
+    socket.on('clear', clear)
+  }, [canvasRef, clear])
 
-    let startPoint = prevPoint ?? currentPoint;
-    ctx.beginPath();
-    ctx.lineWidth = lineWidth;
-    ctx.strokeStyle = lineColor;
-    ctx.moveTo(startPoint.x, startPoint.y);
-    ctx.lineTo(currX, currY);
-    ctx.stroke();
 
-    ctx.fillStyle = lineColor;
-    ctx.beginPath();
-    ctx.arc(startPoint.x, startPoint.y, 2, 0, 2 * Math.PI);
-    ctx.fill();
+  function createLine({ prevPoint, currentPoint, ctx }: Draw) {
+    socket.emit('draw-line', ({ prevPoint, currentPoint, color }))
+    drawLine({ prevPoint, currentPoint, ctx, color })
   }
 
   return (
-    <div className='w-screen h-screen bg-blue- flex justify-center items-center'>
+    <div className='w-screen h-screen bg-blue-700 flex justify-center items-center'>
       <div className='flex flex-col gap-10 pr-10'>
         <ChromePicker color={color} onChange={(e) => setColor(e.hex)} />
         <button type='button' className='p-2 rounded-md border bg-pink-400 text-white border-black' onClick={clear}>
@@ -44,6 +47,15 @@ const Page: FC<PageProps> = ({ }) => {
         height={550}
         className='border border-black rounded-md bg-white'
       />
+      <div className='flex flex-col gap-10 pl-10'>
+
+        <button type='button' className='p-2 rounded-md border bg-pink-400 text-white border-black' onClick={() => {
+          socket.emit('clear')
+        }}>
+          Clear canvas
+        </button>
+      </div>
+
     </div>
   );
 }
